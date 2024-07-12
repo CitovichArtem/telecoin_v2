abf9c8c64af4ebe3b591da7f59f74d2a284b178c
CitovichArtem <citovichartem@gmail.com>
2024-07-12 14:49:53 +0300

    попытка добавить видео
    но вроде что-то с лоадером

--- src/components/Game/GamePage.js
+++ src/components/Game/GamePage.js
@@ -9,6 +9,7 @@ class GamePage extends React.Component {
                 <img width="50px" src="img/монетка.png"/>
                 <h1>GamePAGE</h1>
                 <p>Раздел в разработке</p>
+                <video id='video3inrow' className="video3inrow" src="./img/3inrow.mp4" autoPlay></video>
             </section>
     )}
 }
