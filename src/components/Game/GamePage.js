fccea1aa2e7ffb6beaf47b8086d5f3076e53f561
CitovichArtem <citovichartem@gmail.com>
2024-07-12 10:53:02 +0300

    1
    1

--- src/components/Game/GamePage.js
+++ src/components/Game/GamePage.js
@@ -6,9 +6,12 @@ class GamePage extends React.Component {
     render() {
         return(
             <section id="GamePage" className='GamePage'>
-                <img width="50px" src="img/монетка.png"/>
-                <h1>GamePAGE</h1>
-                <p>Раздел в разработке</p>
+                <div className='GamePageCont'>
+                    <img width="50px" src="img/монетка.png"/>
+                    <h1>GamePAGE</h1>
+                    <p>Раздел в разработке</p>
+                </div>
+                
             </section>
     )}
 }
