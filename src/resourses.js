d5a9685612983cf6a702659c43f16cc3d4a03703
CitovichArtem <citovichartem@gmail.com>
2024-07-17 11:32:20 +0300

    исправление косяков

--- src/resourses.js
+++ src/resourses.js
@@ -4,7 +4,7 @@ let leagues = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Epic', 'Legen
 let arr;    
 let arrString = localStorage.getItem('arr');
 console.log(arrString);
-if (arrString) {
+if (!arrString) {
     // Если данные есть, преобразуем их обратно в Map
     arr = new Map(Object.entries(JSON.parse(arrString)));
 
@@ -12,13 +12,13 @@ if (arrString) {
     // Если данных нет, создаем новый объект Map и задаем начальные значения
     arr = new Map();
     arr
-        .set('balance', '10000')
-        .set('profitTap', '1')
+        .set('balance', 10000)
+        .set('profitTap', 1)
         .set('moneyToUp', '25K')
-        .set('profitHour', '0')
+        .set('profitHour', 0)
         .set('myLeague', 'Bronze')
-        .set('energy', '1000')
-        .set('energyLimit', '1000');
+        .set('energy', 1000)
+        .set('energyLimit', 1000);
 
     // Преобразуем Map в объект и сохраняем в localStorage
     const obj = Object.fromEntries(arr);
