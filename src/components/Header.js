5cb7e0738bd5e94b4022e2bc94fc534a61e65aa5
CitovichArtem <citovichartem@gmail.com>
2024-07-18 13:05:41 +0300

    попытки
    не получилось достать из тг апи данные, здесь пытаюсь найти их

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,19 @@ import ReactDOMClient from 'react-dom/client';
 import { IoPersonSharp } from "react-icons/io5";
 import tg from "../resourses.js";
 let username1;
-if(tg){
-    username1 = tg.username ;
-}else{
-    username1 = "Имя Фамилия)";
-}
+
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        if(tg){
+            username1 = tg.username ;
+            console.log(username1);
+            console.log(tg.username, ',,', tg.WebAppUser.username, ',,', tg.WebAppUser.first_name, ' ,', tg.last_name);
+            username1 = tg.username + ',1,' + tg.WebAppUser.username + ',2,' +  tg.WebAppUser.first_name + ',3,' + tg.last_name;
+        }else{
+            username1 = "Имя Фамилия)";
+        }
         this.state = {
             username: username1,
         }
